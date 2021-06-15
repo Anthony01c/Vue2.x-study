7ed0833d577cddaf0f55952cea3ad407ee62fb6a
Lukas <yangmingdada@gmail.com>
2021-06-15 12:46:21 +0800

    为自定义事件 保存副本

---
+++ my-project/src-自定义事件/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+<!--       <Header @addTodo="addTodo"/>   &lt;!&ndash; //给当前的header对象绑定监听&ndash;&gt;-->
+      <Header ref='header'/>
+      <List :todos="todos"
+              :deleteTodo="deleteTodo"
+              :updateTodo="updateTodo"/>
+        <Footer :todos="todos"
+                :clearCompletedTodos="clearCompletedTodos"
+                :checkAll="checkAll"
+                />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from "./components/Footer";
+import List from "./components/List";
+import {saveTodos,readTodos} from "./utils/stroageUtils";
+
+export default {
+  name: 'app',
+  methods:{
+    addTodo(todo){
+      this.todos.unshift(todo)
+    },
+    deleteTodo(index){
+      this.todos.splice(index,1)
+    },
+    clearCompletedTodos(){
+      this.todos = this.todos.filter((todo,index)=> !todo.completed)
+    },
+    checkAll(isCheckAll){
+      this.todos.forEach(todo => todo.completed = isCheckAll)
+    },
+    updateTodo(todo,isCheck){
+      todo.completed = isCheck
+    }
+  },
+  components: {Footer, List, Header},
+  data () {
+    return {
+      todos:[]
+    }
+  },
+  mounted() {
+    this.$refs.header.$on('addTodo',this.addTodo)
+    //模拟异步
+    setTimeout(()=>{
+      //读取local中保存的todos，更新数据
+      this.todos = readTodos()
+    },1000);
+  },
+  watch:{
+    todos:{
+      deep:true,//深度监视（本身和内部所有层次的数据）
+      /*handler(value){
+        //将todos保存到local中（以json格式）
+        saveTodos(value);
+      }*/
+      handler:saveTodos
+
+    }
+  }
+}
+</script>
+
+<style>
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ my-project/src-自定义事件/components/Footer.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="todo-footer">
+    <label>
+      <input type="checkbox" v-model="isCheckAll"/>
+    </label>
+    <span>
+          <span>已完成{{compSize}}</span> / {{ todos.length }}
+        </span>
+    <button class="btn btn-danger" v-show="compSize>0"
+            @click="clearCompletedTodos">清除已完成任务</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props:{
+    todos:Array,
+    clearCompletedTodos:Function,
+    checkAll:Function
+
+  },
+  computed:{
+    compSize(){
+      return this.todos.reduce((preTotal,todo,index)=>preTotal+(todo.completed ? 1 : 0),0)
+    },
+    isCheckAll:{
+      get(){
+        return this.todos.length === this.compSize  && this.compSize > 0
+        //读属性值就会自动调用对应的getter方法
+      },
+     set(value){
+      this.checkAll(value)
+     }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.todo-footer {
+  height: 40px;
+  line-height: 40px;
+  padding-left: 6px;
+  margin-top: 5px;
+}
+
+.todo-footer label {
+  display: inline-block;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.todo-footer label input {
+  position: relative;
+  top: -1px;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+
+.todo-footer button {
+  float: right;
+  margin-top: 5px;
+}
+</style>
